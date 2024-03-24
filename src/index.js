ea2e8606a113fa9a1b359c38dd9991a8e5f74f08
yiranshu <shuyiran1425@hotmail.com>
2024-03-23 23:03:51 -0400

    movies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event)=>{
+        event.preventDefault()
+        const input = document.querySelector('input#searchByID')
+        console.log(event)
+        console.log(input.value)
+
+    fetch(`http://localhost:3002/movies/${input.value}`)
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data)
+            //here we store and present in html
+            const title = document.querySelector('section#movieDetails h4')
+            title.innerText = data.title
+            const summary = document.querySelector('section#movieDetails p')
+            summary.innerText = data.summary
+        })  
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
